36fd7de232e3ea30b7f8b160b858aa8c8eb8c776
elbotas <elbotas@whitePearl>
2014-05-19 23:04:37 +0100

    User complete

---
+++ ebayPT/src/ebayPT/User.java
@@ -0,0 +1,35 @@
+package ebayPT;
+
+public class User implements IUser {
+
+	private String username, email, name;
+	private IUserType type;
+	
+	public User(IUserType type, String username, String email, String name){
+		this.type = type;
+		this.username = username;
+		this.email = email;
+		this.name = name;
+	}
+	
+	@Override
+	public String getEmail() {
+		return this.email;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean equals(IUser user) {
+		return user.getUsername().equals(this.username);
+	}
+
+}

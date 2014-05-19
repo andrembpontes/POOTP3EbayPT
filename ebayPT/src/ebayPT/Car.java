01889d9b75014c62d5c81bef7bf10587a9ab6832
andrembpontes <andrembpontes@gmail.com>
2014-05-19 23:25:09 +0100

    added car

---
+++ ebayPT/src/ebayPT/Car.java
@@ -0,0 +1,32 @@
+package ebayPT;
+
+public class Car extends Product implements ICar {
+
+	private String make, model;
+	private int year;
+	
+	Car(EProductCategory category, String code, String description,
+			EProductState state, String make, String model, int year) {
+		super(category, code, description, state);
+		
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
+	
+	@Override
+	public String getMake() {
+		return this.make;
+	}
+
+	@Override
+	public String getModel() {
+		return this.model;
+	}
+
+	@Override
+	public int getYear() {
+		return this.year;
+	}
+
+}

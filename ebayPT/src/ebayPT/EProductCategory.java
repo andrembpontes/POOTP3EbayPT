765730fcddea1cbc5ed46f697eb4a4d3607537d5
andrembpontes <andrembpontes@gmail.com>
2014-05-20 23:24:48 +0100

    EProductCategory commented

--- ebayPT/src/ebayPT/EProductCategory.java
+++ ebayPT/src/ebayPT/EProductCategory.java
@@ -1,6 +1,13 @@
 package ebayPT;
 
+/**
+ * Products category enumeration containing all possible categories for products
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ *
+ */
 public enum EProductCategory {
-	TABLET,
-	CAR;
+			//Categories list
+	TABLET,	//Tablet
+	CAR;	//Car
 }

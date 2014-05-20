e3ed2a792c04c68ec591cd41d5054bc7bc94804d
andrembpontes <andrembpontes@gmail.com>
2014-05-20 23:26:15 +0100

    EProductState commented

--- ebayPT/src/ebayPT/EProductState.java
+++ ebayPT/src/ebayPT/EProductState.java
@@ -1,7 +1,14 @@
 package ebayPT;
 
+/**
+ * Enumeration containing all possible product states
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ *
+ */
 public enum EProductState {
-	SALE,
-	AUCTION,
-	SOLD;
+				//Product states enumeration:
+	SALE,		//Sale
+	AUCTION,	//Auction
+	SOLD;		//Sold
 }

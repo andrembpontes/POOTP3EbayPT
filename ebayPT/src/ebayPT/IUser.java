608e92d7699df7f02391d208b8894f9e488a20ea
rodjbs <rodjbs@gmail.com>
2014-05-20 23:24:00 +0100

    IUser commented

--- ebayPT/src/ebayPT/IUser.java
+++ ebayPT/src/ebayPT/IUser.java
@@ -1,14 +1,39 @@
 package ebayPT;
 
+/**
+ * An IUser represents a user in the ebayPT system.
+ *
+ * @author n42540: Rodrigo Simões; n42845: André Pontes
+ */
 public interface IUser {
-	
+	/**
+	 * Gets the email of this user.
+	 * @return user email
+	 */
 	String getEmail();
 	
+	/**
+	 * Gets the name of this user.
+	 * @return user's name
+	 */
 	String getName();
 	
+	/**
+	 * Gets this user's username.
+	 * @return username
+	 */
 	String getUsername();
 	
+	/**
+	 * Gets this user's type
+	 * @return user's type
+	 */
 	IUserType getUserType();
 	
+	/**
+	 * Returns whether this IUser is the same as <code>user</code>.
+	 * @param user an IUser against which this IUser will be compared
+	 * @return true if this IUser equals <code>user</code>, false otherwise
+	 */
 	boolean equals(IUser user);
 }

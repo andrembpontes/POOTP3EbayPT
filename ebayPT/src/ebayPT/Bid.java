dc91660b2e5d996a75805b42589d18802bfa8530
andrembpontes <andrembpontes@gmail.com>
2014-05-20 17:37:47 +0100

    Bid implements Comparable; IBid and Bid commented

--- ebayPT/src/ebayPT/Bid.java
+++ ebayPT/src/ebayPT/Bid.java
@@ -1,10 +1,22 @@
 package ebayPT;
 
-public class Bid implements IBid {
+/**
+ * Represents an auction bid, in other words, a pair IUser (bidder) / bid amount
+ * 
+ * @author //TODO specify authors
+ *
+ */
+public class Bid implements IBid, Comparable<IBid>{
 
 	private IUser bidder;
 	private int amount;
 	
+	/**
+	 * Creates new bid
+	 * 
+	 * @param bidder: Bidder user
+	 * @param amount: Bid amount
+	 */
 	public Bid(IUser bidder, int amount){
 		this.bidder = bidder;
 		this.amount = amount;
@@ -18,6 +30,11 @@ public class Bid implements IBid {
 	@Override
 	public int getAmount() {
 		return this.amount;
+	}
+
+	@Override
+	public int compareTo(IBid arg0) {
+		return this.amount - arg0.getAmount();
 	}
 
 }
--- ebayPT/src/ebayPT/IBid.java
+++ ebayPT/src/ebayPT/IBid.java
@@ -1,6 +1,17 @@
 package ebayPT;
 
 public interface IBid {
+	/**
+	 * Returns bidder user
+	 * 
+	 * @return Bidder user
+	 */
 	IUser getBidder();
+	
+	/**
+	 * Returns bid amount
+	 * 
+	 * @return Bid amount
+	 */
 	int getAmount();
 }

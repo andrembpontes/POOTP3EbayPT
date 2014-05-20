60444bfc005d0fbb92fec54d3fd9061ef15aa922
andrembpontes <andrembpontes@gmail.com>
2014-05-20 23:22:13 +0100

    EAction commented

--- ebayPT/src/ebayPT/EAction.java
+++ ebayPT/src/ebayPT/EAction.java
@@ -1,17 +1,25 @@
 package ebayPT;
 
+/**
+ * Enumeration containing all of actions (features) of ebay@Pt and which access
+ * level is required to perform its actions.
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ *
+ */
 public enum EAction implements IAction{
-	LIST_AUCTIONS(EUserType.GUEST),
-	ADD_USER(EUserType.GUEST),
-	LIST_USERS(EUserType.ADMIN),
-	ADD_PRODUCT(EUserType.REGISTERED),
-	CREATE_AUCTION(EUserType.REGISTERED),
-	BID(EUserType.REGISTERED),
-	CLOSE_AUCTION(EUserType.REGISTERED),
-	LIST_PRODUCTS(EUserType.REGISTERED),
-	LIST_BIDS(EUserType.REGISTERED);
+											//Permissions list:
+	LIST_AUCTIONS(EUserType.GUEST),			//List all auctions
+	ADD_USER(EUserType.GUEST),				//Add a new user
+	LIST_USERS(EUserType.ADMIN),			//List all users
+	ADD_PRODUCT(EUserType.REGISTERED),		//Create a new product
+	CREATE_AUCTION(EUserType.REGISTERED),	//Open a new auction
+	BID(EUserType.REGISTERED),				//Bid on an open auction
+	CLOSE_AUCTION(EUserType.REGISTERED),	//Close auctions
+	LIST_PRODUCTS(EUserType.REGISTERED),	//List own products
+	LIST_BIDS(EUserType.REGISTERED);		//List auction bids
 		
-	private EUserType allowedUsers;
+	private EUserType allowedUsers;		//Minimun user access level required
 	
 	private EAction(EUserType allowedUsers){
 		this.allowedUsers = allowedUsers;

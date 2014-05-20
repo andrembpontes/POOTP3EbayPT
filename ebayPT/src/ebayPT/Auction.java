ef623409a918a9c3def38b35894534267db967cb
andrembpontes <andrembpontes@gmail.com>
2014-05-20 17:02:21 +0100

    Auction fix

--- ebayPT/src/ebayPT/Auction.java
+++ ebayPT/src/ebayPT/Auction.java
@@ -1,24 +1,32 @@
 package ebayPT;
 
+import java.util.Collection;
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.TreeSet;
 
 public class Auction implements IAuction {
 
-	private IUser seller;
-	private IProduct product;
-	private List<IBid> bids;
-	private boolean open;
-	private int base;
+	private IUser seller;		//Seller user
+	private IProduct product;	//Product's auction
+	private Collection<IBid> bids;	//Bids list
+	private boolean open;		//True if is open, False is is closed
+	private int base;			//Minimum bid amount
 	
+	/**
+	 * Creates and open a standard auction for givens seller, product and base
+	 * amount.
+	 * 
+	 * @param seller: Seller user
+	 * @param product: Auction product
+	 * @param base: Minimum bid amount
+	 */
 	public Auction(IUser seller, IProduct product, int base){
 		this.seller = seller;
 		this.product = product;
 		
 		this.open = true;
 		
-		this.bids = new LinkedList<IBid>();
+		this.bids = new TreeSet<IBid>(); //TODO cant add duplicated entries
 	}
 	
 	@Override
@@ -57,6 +65,14 @@ public class Auction implements IAuction {
 		this.bids.add(new Bid(user, amount));
 		
 		return false;
+	}
+
+	@Override
+	public IBid getWinnerBid() {
+		if(this.open)
+			return null;
+		
+		return this.bids.iterator().next();
 	}
 
 }
--- ebayPT/src/ebayPT/IAuction.java
+++ ebayPT/src/ebayPT/IAuction.java
@@ -3,13 +3,56 @@ package ebayPT;
 import java.util.Iterator;
 
 public interface IAuction {
+	/**
+	 * Returns seller user
+	 * 
+	 * @return Seller user
+	 */
 	IUser getSeller();
+	
+	/**
+	 * Returns current auction product
+	 * 
+	 * @return Current auction product
+	 */
 	IProduct getProduct();
+	
+	/**
+	 * Creates and returns an iterator for all bids
+	 * 
+	 * @return Iterator for all bids
+	 */
 	Iterator<IBid> getBids();
 	
+	/**
+	 * Verify if auction is open
+	 * Returns True if yes, else False
+	 * 
+	 * @return True if auction is open, False if not
+	 */
 	boolean isOpen();
 	
+	/**
+	 * Close current auction
+	 * 
+	 * @throws NoBidsException: Trying to close an auction that have no bids
+	 */
 	void close() throws NoBidsException;
 	
-	boolean bid(IUser user, int amount) throws LowBidAmountException;
+	/**
+	 * @param user: Bidder user
+	 * @param amount: Bid amount
+	 * @return True if bid become closed by consequence of bid
+	 * @throws LowBidAmountException: Bid amount is lower than base amount
+	 * @throws BiddingClosedAuctionException: Bidding a closed auction
+	 */
+	boolean bid(IUser user, int amount) throws LowBidAmountException,
+	BiddingClosedAuctionException;
+	
+	/**
+	 * Returns winner bid, null if auction is running
+	 * 
+	 * @return Winner bid, Null if there is no winner yet
+	 */
+	IBid getWinnerBid();
 }

23285e12df0b424a47d5835cdffe038b30dc75e8
andrembpontes <andrembpontes@gmail.com>
2014-05-20 23:33:37 +0100

    IAction commented

--- ebayPT/src/ebayPT/IAction.java
+++ ebayPT/src/ebayPT/IAction.java
@@ -1,6 +1,18 @@
 package ebayPT;
 
 
+/**
+ * Represents an action that can be performed in ebay@Pt and respective user
+ * type required to perform its
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ *
+ */
 public interface IAction {
+	/**
+	 * Returns minimum user type required to perform action
+	 * 
+	 * @return Minimum user type required to perform action
+	 */
 	EUserType getAllowedUserType();
 }

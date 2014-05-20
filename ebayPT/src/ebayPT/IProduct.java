e636daffab841e8f3192d009d1a50db822efec68
rodjbs <rodjbs@gmail.com>
2014-05-20 23:28:00 +0100

    IProduct commented

--- ebayPT/src/ebayPT/IProduct.java
+++ ebayPT/src/ebayPT/IProduct.java
@@ -1,13 +1,38 @@
 package ebayPT;
 
+/**
+ * Supertype for all types of products that can be auctioned in ebayPT.
+ *
+ * @author n42540: Rodrigo Simões; n42845: André Pontes
+ */
+
 public interface IProduct {
+	/**
+	 * Initial state of a product.
+	 */
 	static final EProductState START_STATE = EProductState.SALE; 
 	
+	/**
+	 * Gets the category of this product.
+	 * @return category of this product
+	 */
 	EProductCategory getCategory();
 	
+	/**
+	 * Gets the code of this product.
+	 * @return code of this product
+	 */
 	String getCode();
 	
+	/**
+	 * Gets the description of this product.
+	 * @return description of this product
+	 */
 	String getDescription();
 	
+	/**
+	 * Gets state of this product.
+	 * @return state of this product
+	 */
 	EProductState getState();
 }

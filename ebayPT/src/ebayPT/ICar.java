4b7cbb8349a1992d80e1f328e076f7b3000b8383
André Pontes <andrembpontes@gmail.com>
2014-05-22 08:58:08 +0100

    ICar commented

--- ebayPT/src/ebayPT/ICar.java
+++ ebayPT/src/ebayPT/ICar.java
@@ -1,9 +1,26 @@
 package ebayPT;
 
 public interface ICar extends IProduct {
+	
+	/**
+	 *  Get car make
+	 * 
+	 * @return car make
+	 * 
+	 */
 	String getMake();
 	
+	/**
+	 * Get car model
+	 * 
+	 * @return car model
+	 */
 	String getModel();
 	
+	/**
+	 * Get car year
+	 * 
+	 * @return car year
+	 */
 	int getYear();
 }

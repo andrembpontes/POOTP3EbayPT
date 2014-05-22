89b7a9dbc41beef32a7cac8c25508829e9c28551
André Pontes <andrembpontes@gmail.com>
2014-05-22 09:18:40 +0100

    ITablet commented

--- ebayPT/src/ebayPT/ITablet.java
+++ ebayPT/src/ebayPT/ITablet.java
@@ -1,9 +1,25 @@
 package ebayPT;
 
 public interface ITablet extends IProduct {
+	
+	/**
+	 * Return Tablet's brand'
+	 * 
+	 * @return Tablet brand 
+	 */
 	String getBrand();
 	
+	/**
+	 * Return Tablet's size'
+	 * 
+	 * @return Tablet size 
+	 */
 	int getSize();
 	
+	/**
+	 * Return Tablet's weight'
+	 * 
+	 * @return Tablet weight
+	 */
 	int getWeight();
 }

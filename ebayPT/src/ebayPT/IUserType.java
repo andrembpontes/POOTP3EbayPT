abf7e07e1587eda422a3fde67dc4fbd7f48e2a47
André Pontes <andrembpontes@gmail.com>
2014-05-22 09:27:22 +0100

    IUserType commented

--- ebayPT/src/ebayPT/IUserType.java
+++ ebayPT/src/ebayPT/IUserType.java
@@ -1,5 +1,13 @@
 package ebayPT;
+//TODO general comments
 
 public interface IUserType {
+	
+	/**
+	 * Verify if has access level of givens user type
+	 * 
+	 * @param userType: user access level to test
+	 * @return True if has access level, False if not 
+	 */
 	boolean hasAccessLevel(EUserType userType);
 }

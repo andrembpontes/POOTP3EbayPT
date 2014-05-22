cad20dde1413151a4d610bf7ed69e8fdb6153515
André Pontes <andrembpontes@gmail.com>
2014-05-22 09:22:36 +0100

    IUserControl commented

--- ebayPT/src/ebayPT/IUserControl.java
+++ ebayPT/src/ebayPT/IUserControl.java
@@ -1,8 +1,23 @@
 package ebayPT;
 
 public interface IUserControl {
+	
+	/**
+	 * Verify if current logged user can perform givens action
+	 * 
+	 * @param action: Action to test
+	 * @return True if action can be performed, False if not
+	 */
 	boolean isAllowed(EAction action);
 	
+	//TODO add exception to when is already someone logged in
+	/**
+	 * Login with givensuser
+	 */
 	void login(IUser user);
+	
+	/**
+	 * Logout current user
+	 */
 	void logout();
 }

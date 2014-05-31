5e5b31e2a963a14427c7a653977760eb0c9f1fc4
rodjbs <rodjbs@gmail.com>
2014-05-31 15:48:21 +0100

    Fix ComparatorUserBySales; add execution permission to scripts

--- ebayPT/src/ebayPT/ComparatorUserBySales.java
+++ ebayPT/src/ebayPT/ComparatorUserBySales.java
@@ -7,7 +7,7 @@ public class ComparatorUserBySales implements Comparator<IUser> {
 
 	@Override
 	public int compare(IUser o1, IUser o2) {
-		int salesDiff = o1.getSales() - o2.getSales();
+		int salesDiff = o2.getSales() - o1.getSales();
 		
 		if(salesDiff == 0)
 			return o1.getUsername().compareTo(o2.getUsername());

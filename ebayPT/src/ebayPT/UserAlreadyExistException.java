f3010d646a50f301f72af40e852af0e458a49b80
rodjbs <rodjbs@gmail.com>
2014-05-31 16:43:33 +0100

    Comment UserAlreadyExistException

--- ebayPT/src/ebayPT/UserAlreadyExistException.java
+++ ebayPT/src/ebayPT/UserAlreadyExistException.java
@@ -1,11 +1,13 @@
 package ebayPT;
 
+/**
+ * Exception used to signal that a user already exists.
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ */
 
 public class UserAlreadyExistException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long	serialVersionUID	= 1L;
 
 }

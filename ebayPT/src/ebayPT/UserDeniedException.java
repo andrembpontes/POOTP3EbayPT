23790a2039a0d8cd4270ed71b11a87b6b3b48ea9
rodjbs <rodjbs@gmail.com>
2014-05-31 16:37:58 +0100

    UserDeniedException commented

--- ebayPT/src/ebayPT/UserDeniedException.java
+++ ebayPT/src/ebayPT/UserDeniedException.java
@@ -1,11 +1,14 @@
 package ebayPT;
 
+/**
+ * Exception used to signal that the logged user doesn't have permission
+ * to execute an action.
+ * 
+ * @author n42540: Rodrigo Simoes; n42845: Andre Pontes
+ */
 
 public class UserDeniedException extends Exception {
 	
-	/**
-	 * 
-	 */
 	private static final long	serialVersionUID	= 1L;
 	
 	private IUserType neededType;
